6afbf09837668a741bda409560a32ef7faecac1b
tgarg93 <tushargarg93@gmail.com>
2015-11-05 10:47:43 -0500

    Kalman

--- create_controller.py
+++ create_controller.py
@@ -10,12 +10,13 @@ import numpy.matlib
 import sys
 import rospy
 import cv2
-import math
 
 from std_msgs.msg import (
     Header,
     UInt16,
 )
+
+from nav_msgs.msg import Odometry
 
 from geometry_msgs.msg import (
     PoseArray,
@@ -34,61 +35,98 @@ class CreateController(object):
         ROS STUFF
         """
         name = rospy.get_param("~myname")
-        self._fresh=False
+        self._image_fresh=False
+        self._odom_fresh=False
         self._no_detection=True
+        self._last_image_t = None
+        self._last_time = None
+        self._robot_t = None
+        self._robot_R = None
+        self._true_t = None
+        self._true_R = None
+        self.v = 0.0
+        self.omega = 0.0
+        # Data saving
+        #self.savefile = open('~/Desktop/data.csv','ow')
+        # Noise covariances
+        self.Q_t = np.eye(3)
+        self.R_t = np.eye(3)
+        # Start with no informatnoi - origin with very large covariance
+        self.x_t = np.array([[0,0,0]]).T
+        self.P_t = np.array([[10.**6,0,0],[0,10.**6,0],[0,0,10.**6]])
         self._pub = rospy.Publisher("/"+name+"/cmd_vel",Twist,queue_size=10)
-        rospy.Subscriber("/"+name+"/tag_detections_pose",PoseArray,self._pose_callback)
+        rospy.Subscriber("/"+name+"/tag_detections_pose_noisy",PoseStamped,self._noisy_pose_callback)
+        rospy.Subscriber("/"+name+"/tag_detections_pose",PoseStamped,self._ground_truth_pose_callback)
 
-    def _pose_callback(self,posearray):
+    def _noisy_pose_callback(self,posearray):
         """
         ROS STUFF
         """
-        if not posearray.poses:
+        if self._last_image_t is not None:
+            self._image_dt = posearray.header.stamp - self._last_image_t
+        else:
+            self._image_dt = 0
+        self._last_image_t = posearray.header.stamp
+        self._image_fresh = True
+        if posearray.pose.position.z==0:
             self._robot_t = None
             self._robot_R = None
-            self._fresh = True
             self._no_detection = True
             return
-        (marker_t, marker_R) = get_t_R(posearray.poses[0])
-        # Compensate for tilt of camera
-        angle = 12*np.pi/180
-        camera_tilt=np.array([[1,0,0,0],[0,np.cos(angle),-np.sin(angle),0],[0,np.sin(angle),np.cos(angle),0],[0,0,0,1]])
-        marker_t = np.dot(camera_tilt,marker_t)
-        marker_R = np.dot(camera_tilt,marker_R)
-        # Put everything in homogeneous form
-        camera2marker = marker_R
-        camera2marker[0:3,3] = marker_t[0:3].T
-        print camera2marker
-        # Transform from camera to robot
-        camera2robot=np.linalg.inv(np.array([[0,0,1,0],[-1,0,0,-0.09],[0,-1,0,0],[0,0,0,1]]))
-        # Transform from final pose to marker
-        final2marker = np.array([[0,0,-1,0],[-1,0,0,0],[0,1,0,0],[0,0,0,1]])
-        # Put it all together
-        # Want: final to robot
-        final_hom = np.dot(np.dot(final2marker,np.linalg.inv(camera2marker)),camera2robot)
-        print final_hom
-        self._robot_t = np.copy(final_hom[0:3,3])
-        self._robot_R = final_hom
-        self._robot_R[0:3,3] = 0
-        print self._robot_t
-        self._fresh = True
+        
+        (self._robot_t, self._robot_R) = get_t_R(posearray.pose)
         self._no_detection = False
+
+    def _ground_truth_pose_callback(self,posearray):
+        """
+        ROS STUFF
+        """
+        if posearray.pose.position.z==0:
+            self._true_t = None
+            self._true_R = None
+            return
+        (self._true_t, self._true_R) = get_t_R(posearray.pose)
+  
+    def get_ground_truth_pose(self):
+        """
+        Gives ground truth pose when noisy marker pose is given. Returns (x,y,theta) as
+        3x1 numpy array. Returns None if no new detection is found
+        """
+        if self._no_detection:
+            return None
+        # Angle is yaw
+        angle = np.arctan2(self._true_R[1,0],self._true_R[0,0])
+        dx = self._true_t[0,0]
+        dy = self._true_t[1,0]
+        return np.array([[dx,dy,angle]]).T
+
+    def F_matrix(self,dt,v,theta_t):
+        """
+        F matrix given in the prediction equations for the covariance estimate.
+        Input: dt - time between two prediction steps (probably 1/60.0)
+               v - norm of the velocity of the robot at current time
+               theta_t - angle estimated at current time
+        Output: 3x3 numpy array F
+        """
+        return np.array([[1, 0, -dt*v*np.sin(theta_t)],
+                         [0, 1,  dt*v*np.cos(theta_t)],
+                         [0, 0,                     1]])
 
     def get_marker_pose(self):
         """
-        If a detection is found, returns the robot x,y and theta in the marker frame, as well
-        as a boolean to determine if a new detection has arrived.
+        If a detection is found, returns the robot (x,y and theta) in the marker
+        frame as an numpy array, as well as a boolean to determine if a new detection has arrived.
         If a new detection has arrived but no tags were found, returns all None.
         """
         if self._no_detection:
-            return None, None, None, None
+            return None, None
         # Angle is yaw
         angle = np.arctan2(self._robot_R[1,0],self._robot_R[0,0])
-        dx = self._robot_t[0]
-        dy = self._robot_t[1]
-        fresh = self._fresh
-        self._fresh=False
-        return dx,dy,angle, fresh
+        dx = self._robot_t[0,0]
+        dy = self._robot_t[1,0]
+        fresh = self._image_fresh
+        self._image_fresh=False
+        return np.array([[dx,dy,angle]]).T, fresh
 
     def command_velocity(self,vx,wz):
         """
@@ -100,42 +138,22 @@ class CreateController(object):
         twist.angular.z = wz
         self._pub.publish(twist)
 
-    def command_create(self, goal_x=0, goal_y=0):
+    def command_create(self):
+        MAX_SPEED=0.1
         """
         YOUR CODE HERE
         This function is called at 60Hz. At each iteration, check if a fresh measurement has come in.
         If so, use your controller to move the create according to the robot pose.
         """
+        dt = None
+        if self._last_time is not None:
+            dt = (rospy.Time.now() - self._last_time).to_sec()
+        self._last_time = rospy.Time.now()
 
-        MAX_SPEED=0.1
-        x, y, theta, fresh = self.get_marker_pose()
-        x -= goal_x
-        y -= goal_y
-        kp=0.5
-        ka=0.5
-        kb=0
-
-        if fresh is None:
-            self.command_velocity(0, 0)
-            return
-
-        if not fresh:
-            return
-
-        rho = np.sqrt(x*x + y*y)
-        beta = -math.atan2(-y, -x)
-
-        alpha = -beta - theta
-        if alpha < -np.pi:
-            alpha += 2 * np.pi
-        if alpha > np.pi:
-            alpha -= 2 * np.pi
-
-        v = kp * rho
-        w = ka * alpha + kb * beta
-
-        self.command_velocity(min(v, MAX_SPEED), w)
-
+        # To save out data - for grading purposes
+        # g = self.get_ground_truth_pose()
+        # self.savefile.write('%f,%f,%f,%f,%f,%f\n' % self.x_t[0,0], self.x_t[1,0], self.x_t[2,0], g[0,0], g[1,0], g[2,0]) 
+        return
 
 def main(args):
     rospy.init_node('create_controller')
@@ -152,3 +170,4 @@ if __name__ == "__main__":
     try:
         main(sys.argv)
     except rospy.ROSInterruptException: pass
+
